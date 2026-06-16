b5849b57af1eb052f6fb39a87669a6e9a949f29c
stjhin <stjhin@gmail.com>
2026-06-16 13:07:45 -0400

    fix base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stjhin.github.io',
+  base: '/pluto/',  // <-- Is this line here or not?
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
