623f0364a9ec16a5985be446c5175d469edc60b0
stjhin <stjhin@gmail.com>
2026-06-16 11:42:56 -0400

    deployment update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://github.com/stjhin/pluto',
+  base: '/pluto/',  // Replace with your repo name
   vite: {
     plugins: [tailwindcss()]
   }
